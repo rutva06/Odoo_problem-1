a024a36d017f3946542d25425d033f88dd6dae44
Rutva Prajapati <rutvaprajapati66@gmail.com>
2026-07-12 10:45:35 +0530

    Add manifest file for EcoSphere ESG Management

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'EcoSphere ESG Management',
+    'version': '1.0',
+    'summary': 'ESG Management Platform',
+    'author': 'Your Team Name',
+    'depends': ['base'],
+    'installable': True,
+}
